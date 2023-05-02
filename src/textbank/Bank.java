5f387b822685723d3a33ffbc9fe05ec02bf7074b
fritziyeke12 <fritziyeke@gmail.com>
2023-05-02 20:34:48 +0100

    base menu options added

---
+++ src/textbank/Bank.java
@@ -0,0 +1,15 @@
+package textbank;
+import java.util.Scanner;
+public class Bank {
+
+    public static void main(String[] args){
+        System.out.println("Bank Admin Menu:");
+        System.out.println("Please Enter a Menu Option");
+        System.out.println("(1): Add Customer to Bank");
+        System.out.println("(2): Change Customer Name");
+        System.out.println("(3): Check Account Balance");
+        System.out.println("(4): Modify Account Balance");
+        System.out.println("(5): Summary of All Accounts");
+        System.out.println("(-1): Quit");
+    }
+}
